e6d06b65feffa77a97361b3ea6ae112cf1ee23e6
Edwin J. Estrella Ayala <edwinjestrella@yahoo.com>
2020-04-20 23:17:17 -0400

    Added devise

---
+++ app/models/uder.rb
@@ -0,0 +1,6 @@
+class Uder < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
